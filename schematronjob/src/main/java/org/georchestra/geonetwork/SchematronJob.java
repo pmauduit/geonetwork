4a5d5cf33cc3a5563d11a0c86a2f7ea671e36f32
Pierre Mauduit <pierre.mauduit@gmail.com>
2017-05-20 19:36:41 +0200

    schematron job - autowiring now works
    Lesson learned: need a specific job factory which would handle the
    autowiring things automatically, factory which actually already exists
    in GN, see org.fao.geonet.util.spring.AutowiringSpringBeanJobFactory
    class.

--- schematronjob/src/main/java/org/georchestra/geonetwork/SchematronJob.java
+++ schematronjob/src/main/java/org/georchestra/geonetwork/SchematronJob.java
@@ -5,60 +5,60 @@ import static org.fao.geonet.repository.specification.MetadataSpecs.isHarvested;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.apache.log4j.Logger;
 import org.fao.geonet.domain.Metadata;
 import org.fao.geonet.kernel.DataManager;
 import org.fao.geonet.repository.MetadataRepository;
-import org.fao.geonet.repository.MetadataRepositoryCustom;
-import org.fao.geonet.utils.Log;
 import org.jdom.Document;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SchematronJob extends QuartzJobBean {
 
-    private String modulename = this.getClass().getName();
     private AtomicBoolean started = new AtomicBoolean(false);
-    
+    private final Logger Log = Logger.getLogger(this.getClass());
+
     @Autowired
-    private ConfigurableApplicationContext applicationContext;
+    private ApplicationContext applicationContext;
     @Autowired
     private DataManager _dataManager;
 
     private String defaultLanguage = "eng";
-    
+
     @Override
-    protected void executeInternal(final JobExecutionContext jobExecContext)
-            throws JobExecutionException {
-    	if (started.get() == true) {
-    		Log.info(modulename,
-    				"Job already running, skipping execution"
-    				);
-    		return;
-    	}
+    protected void executeInternal(final JobExecutionContext jobExecContext) throws JobExecutionException {
+        if (started.get() == true) {
+            Log.info("Job already running, skipping execution");
+            return;
+        }
         try {
-        	started.set(true);
-        	if ((applicationContext == null) || (_dataManager == null)) {
-        		Log.error(modulename, "applicationContext or _dataManager is null, skipping execution");
-        	}
-        	MetadataRepositoryCustom mdrepocustom = applicationContext.getBean(MetadataRepositoryCustom.class);
-        	MetadataRepository mdrepo = applicationContext.getBean(MetadataRepository.class);
-        	List<Integer> mdToValidate = mdrepocustom.findAllIdsBy(isHarvested(false));
-        	for (Integer mdId : mdToValidate) {
-        		Metadata record = mdrepo.findOne(mdId);
-        		try {
-					_dataManager.doValidate(record.getDataInfo().getSchemaId(),
-							mdId.toString(),
-					        new Document(record.getXmlData(false)), defaultLanguage);
-				} catch (Exception e) {
-	        		Log.error(modulename, "Error validating metadata id " + record.getUuid());
-				}
-        	}
+            started.set(true);
+
+            if ((applicationContext == null) || (_dataManager == null)) {
+                Log.error("applicationContext or _dataManager is null, skipping execution");
+                return;
+            }
+            MetadataRepository mdrepo = applicationContext.getBean(MetadataRepository.class);
+            List<Integer> mdToValidate = mdrepo.findAllIdsBy(isHarvested(false));
+            for (Integer mdId : mdToValidate) {
+                Metadata record = mdrepo.findOne(mdId);
+                try {
+                    _dataManager.doValidate(record.getDataInfo().getSchemaId(), mdId.toString(),
+                            new Document(record.getXmlData(false)), defaultLanguage);
+                } catch (Exception e) {
+                    Log.error("Error validating metadata id " + record.getUuid());
+                }
+            }
+        } catch (Exception e) {
+            Log.error("Exception occured: ", e);
         } finally {
-        	Log.info(modulename, "Finished validation job");
-        	started.set(false);
+            Log.info("Finished validation job");
+            started.set(false);
         }
     }
 }
